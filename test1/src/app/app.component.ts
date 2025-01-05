df9284b290b7139979e6d468c20b428cafcb3f8f
Risto Kizov <ristokizov@gmail.com>
2025-01-05 20:18:16 +0100

    Domasna6 - ARIA

--- test1/src/app/app.component.ts
+++ test1/src/app/app.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CompComponent } from './comp/comp.component';
-import { DRIVERS } from '../db-data';
 import { CommonModule } from '@angular/common';
+import { driver } from '.././driver';
+import { DRIVERS } from '.././db-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CompComponent, CommonModule],
+  standalone: true,
+  imports: [CompComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test1';
+  vozaci: driver[] = DRIVERS;
 
-  PB = DRIVERS[0];
-  JM = DRIVERS[1];
-  MM = DRIVERS[2];
-
-  vozaci = DRIVERS;
-  onAppView(){
-    console.log("App e f-ja");
+  onAppView() {
+    console.log('Ne raboti ');
   }
 }
--- test1/src/app/comp/comp.component.ts
+++ test1/src/app/comp/comp.component.ts
@@ -1,18 +1,35 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { driver } from '../../driver';
 
 @Component({
   selector: 'app-comp',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './comp.component.html',
-  styleUrl: './comp.component.css',
+  styleUrls: ['./comp.component.css'],
 })
 export class CompComponent {
-  @Input()
-  vozac: driver | undefined;
-  @Input()
-  indeks: number | undefined;
-  onAppView() {
-    console.log('me klikna');
+  @Input() vozac: driver | undefined;
+  @Input() indeks: number | undefined;
+
+  stilovi() {
+    return {
+      'driver-name': true,
+      highlighted: this.vozac?.category === 'EXPERT',
+    };
+  }
+
+  klasi() {
+    return {
+      'driver-card': true,
+      expert: this.vozac?.category === 'EXPERT',
+      lunatic: this.vozac?.category === 'LUNATIC',
+      asd: this.vozac?.category === 'ASD',
+    };
+  }
+
+  onDrView() {
+    console.log('Details clicked for driver:', this.vozac?.name);
   }
 }
